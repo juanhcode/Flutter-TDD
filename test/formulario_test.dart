bb4071b62ccfbbcd3f0b3ccb1225ccfbdc8bbae8
Juan Hoyos <juanhoyos1347@gmail.com>
2025-09-01 10:31:32 -0500

    Feat: red

--- test/formulario_test.dart
+++ test/formulario_test.dart
@@ -1,24 +1,11 @@
-import 'package:flutter_application_without_tdd/Form_validator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("Validación de formulario", () {
     test("Formulario válido", () {
-      final validator = FormValidator();
+      final validator = FormValidator(); // <-- aún no existe
       final result = validator.validar("Juan", "juan@correo.com");
       expect(result, "Formulario válido");
-    });
-
-    test("Correo vacío", () {
-      final validator = FormValidator();
-      final result = validator.validar("Juan", "");
-      expect(result, "Errores en el formulario");
-    });
-
-    test("Correo inválido", () {
-      final validator = FormValidator();
-      final result = validator.validar("Juan", "juancorreo.com");
-      expect(result, "Errores en el formulario");
     });
   });
 }
